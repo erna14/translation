045cbef6d94f1e434aa7cf9f8391dce3f03627d7
sara <sara12travnik@gmail.com>
2024-03-25 10:49:44 +0100

    final

--- src/app/cookie.js
+++ src/app/cookie.js
@@ -4,8 +4,3 @@ import { getCookie } from "cookies-next";
 export const setCookies = (name, value, days) => {
   setCookie(name, value);
 };
-
-export const getCookies = (name) => {
-  console.log(name);
-  return getCookie("key") || "";
-};
--- src/middleware.js
+++ src/middleware.js
@@ -1,17 +1,15 @@
 import { NextResponse } from "next/server";
-import {getCookies, setCookies} from './app/cookie'
+import { getCookies, setCookies } from "./app/cookie";
+import { cookies } from "next/headers";
 let locales = ["en", "bs"];
 
-// Get the preferred locale, similar to the above or using a library
 function getLocale(request) {
-  // Pokušaj pročitati jezik iz kolačića
-  const cookieLang = getCookies("lang");
-
+  // const cookieLang = getCookies("lang");
+  const cookieLang = request.cookies.get('lang')?.value;
   if (cookieLang && locales.includes(cookieLang)) {
     return cookieLang;
   }
 
-  // Inače, koristi Accept-Language header
   const acceptLanguage = request.headers.get("Accept-Language");
   const preferredLocale = acceptLanguage
     ? acceptLanguage.split(",")[0].trim()
@@ -19,29 +17,20 @@ function getLocale(request) {
   return locales.includes(preferredLocale) ? preferredLocale : "en";
 }
 export function middleware(request) {
-  // Provjeri postoji li podržani jezik u URL-u
   const { pathname } = request.nextUrl;
   const pathnameHasLocale = locales.some(
     (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
   );
- 
+
   if (pathnameHasLocale) return;
 
-  // Preusmjeri ako nema jezika u URL-u
   const locale = getLocale(request);
   request.nextUrl.pathname = `/${locale}`;
-  
-  // Postavi jezik u kolačić
-  setCookies('lang', locale, 365); // Postavi kolačić na 365 dana ili na željenu trajnost
+
+  setCookies("lang", locale, 365);
   return NextResponse.redirect(request.nextUrl);
 }
 
-
 export const config = {
-  matcher: [
-    // Skip all internal paths (_next)
-    "/((?!_next).*)",
-    // Optional: only run on root (/) URL
-    // '/'
-  ],
+  matcher: ["/((?!_next).*)"],
 };
