4a3d5fb5df0b64347a333a4d70904c42d78fd7b0
Erna Berbic <ernaeniberbic@gmail.com>
2024-03-25 09:28:25 +0100

    hahahahahaha

--- src/middleware.js
+++ src/middleware.js
@@ -1,40 +1,4 @@
 import { NextResponse } from "next/server";
-<<<<<<< HEAD
- 
-let locales = ['en', 'bs']
- 
-// Get the preferred locale, similar to the above or using a library
-function getLocale(request) {
-  const acceptLanguage = request.headers.get('Accept-Language');
-  const preferredLocale = acceptLanguage ? acceptLanguage.split(',')[0].trim() : 'en';
-  return locales.includes(preferredLocale) ? preferredLocale : 'en';
-}
- 
-export function middleware(request) {
-  // Check if there is any supported locale in the pathname
-  const { pathname } = request.nextUrl
-  const pathnameHasLocale = locales.some(
-    (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
-  )
- 
-  if (pathnameHasLocale) return
-  // Redirect if there is no locale
-  const locale = getLocale(request)
-  request.nextUrl.pathname = `/${locale}`
-  // e.g. incoming request is /products
-  // The new URL is now /en-US/products
-  return NextResponse.redirect(request.nextUrl)
-}
- 
-export const config = {
-  matcher: [
-    // Skip all internal paths (_next)
-    '/((?!_next).*)',
-    // Optional: only run on root (/) URL
-    // '/'
-  ],
-}
-=======
 import {getCookies, setCookies} from './app/cookie'
 let locales = ["en", "bs"];
 
@@ -81,4 +45,3 @@ export const config = {
     // '/'
   ],
 };
->>>>>>> f8d0aa27d533541ea2377c3a4870e96162bf574a
